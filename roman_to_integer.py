fd3eae691509101e7dc1e3ff8f6a9fe9c6843e93
TheusLopes01 <mslopesilva01@gmail.com>
2025-03-27 15:05:45 -0300

    first commit

---
+++ roman_to_integer.py
@@ -0,0 +1,22 @@
+romans = {
+    'I': 1, 
+    'V': 5, 
+    'X': 10, 
+    'L': 50, 
+    'C': 100, 
+    'D': 500, 
+    'M': 1000, 
+}
+
+s = 'MCMXCIV'
+romanoList = list(s)
+sum = 0
+
+for i in range(len(romanoList)-1):
+    if romans.get(romanoList[i]) < romans.get(romanoList[i+1]):
+        sum -= romans.get(romanoList[i])
+    else:
+        sum += romans.get(romanoList[i])
+sum += romans.get(romanoList[-1])
+print(sum)
+
